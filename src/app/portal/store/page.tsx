6efe2147f5e0232a1bc881802c3e783fe4ab0bcf
Geeneth Punchihewa <geenethpunchihewa@gmail.com>
2025-03-14 14:50:34 +0530

    fix error in store page and product grid

--- src/app/portal/store/page.tsx
+++ src/app/portal/store/page.tsx
@@ -184,6 +184,7 @@ export default function MerchStore() {
                     currentPage={currentPage}
                     totalPages={totalPages}
                     onPageChange={handlePageChange}
+                    isAdmin={isAdmin}
                   />
                 </>
               ) : (
