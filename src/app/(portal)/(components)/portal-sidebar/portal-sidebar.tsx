21d9bf1d08c2465a1771671c371124e6bc7e7994
Geeneth Punchihewa <geenethpunchihewa@gmail.com>
2025-06-24 09:11:46 +0530

    disabled message

--- src/app/(portal)/(components)/portal-sidebar/portal-sidebar.tsx
+++ src/app/(portal)/(components)/portal-sidebar/portal-sidebar.tsx
@@ -72,6 +72,7 @@ const navigationData: NavigationItem[] = [
     label: 'Gala Seating',
     icon: IconArmchair,
     adminOnly: false,
+    disabledMessage: 'Coming Soon!'
   },
   {
     link: '/admin/gala',
